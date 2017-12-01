b9a4dc3fcbed049a82bed88dc12cc24e71f6600d
David Edmundson <kde@davidedmundson.co.uk>
2017-12-01 15:59:37 +0100

    first

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <KToolInvocation>
+#include <KWindowSystem>
+
+#include <QCoreApplication>
+#include <QTest>
+#include <QProcess>
+
+
+class DaveTest : public QObject
+{
+    Q_OBJECT
+public:
+    DaveTest();
+private slots:
+    void testQProcess();
+    void testKRun();
+private:
+    void initServer();
+    void waitForWindow();
+};
+
+DaveTest::DaveTest()
+{
+}
+
+void DaveTest::testQProcess()
+{
+    initServer();
+    QBENCHMARK {
+        QProcess::startDetached("dolphin");
+        waitForWindow();
+    };
+    QProcess::execute("killall -9 dolphin");
+    QTest::qSleep(1000);
+}
+
+void DaveTest::testKRun()
+{
+    initServer();
+    QBENCHMARK {
+        int pid;
+        KToolInvocation::startServiceByDesktopName("dolphin", QString(), 0, 0, &pid, QByteArray(), true);
+        waitForWindow();
+    }
+    QProcess::execute("killall -9 dolphin");
+    QTest::qSleep(1000);
+}
+
+
+
+void DaveTest::waitForWindow()
+{
+    QEventLoop e;
+    connect(KWindowSystem::self(), &KWindowSystem::windowAdded, &e, &QEventLoop::quit);
+    e.exec();
+}
+
+void DaveTest::initServer()
+{
+}
+
+QTEST_GUILESS_MAIN(DaveTest);
+
+#include "main.moc"
---
+++ main_wayland.cpp
@@ -0,0 +1,97 @@
+#include <KWayland/Server/display.h>
+#include <KWayland/Server/datadevicemanager_interface.h>
+#include <KWayland/Server/output_interface.h>
+#include <KWayland/Server/compositor_interface.h>
+#include <KWayland/Server/shell_interface.h>
+#include <KWayland/Server/seat_interface.h>
+
+#include <KToolInvocation>
+
+#include <QCoreApplication>
+#include <QTest>
+#include <QProcess>
+
+
+class DaveTest : public QObject
+{
+    Q_OBJECT
+public:
+    DaveTest();
+private slots:
+    void testQProcess();
+    void testKRun();
+private:
+    void initServer();
+    void waitForWindow();
+    KWayland::Server::ShellInterface *m_shell = nullptr;
+    KWayland::Server::Display *m_display = nullptr;
+};
+
+DaveTest::DaveTest()
+{
+}
+
+void DaveTest::testQProcess()
+{
+    initServer();
+    QBENCHMARK {
+        QProcess::startDetached("dolphin");
+        waitForWindow();
+    }
+    //will crash dolphin which is fine
+    delete m_display;
+    QTest::qSleep(1000);
+}
+
+void DaveTest::testKRun()
+{
+    initServer();
+    QBENCHMARK {
+        int pid;
+        KToolInvocation::startServiceByDesktopName("dolphin", QString(), 0, 0, &pid, QByteArray(), true);
+        waitForWindow();
+    }
+    //will crash dolphin which is fine
+    delete m_display;
+    QTest::qSleep(1000);
+}
+
+
+
+void DaveTest::waitForWindow()
+{
+    QEventLoop e;
+    connect(m_shell, &KWayland::Server::ShellInterface::surfaceCreated,
+            &e, &QEventLoop::quit);
+    e.exec();
+}
+
+void DaveTest::initServer()
+{
+    qDebug() << "init server";
+    using namespace KWayland::Server;
+    m_display = new Display;
+    m_display->setSocketName(QStringLiteral("asf"));
+    m_display->start();
+    DataDeviceManagerInterface *ddm = m_display->createDataDeviceManager();
+    ddm->create();
+    CompositorInterface *compositor = m_display->createCompositor(m_display);
+    compositor->create();
+    m_shell = m_display->createShell();
+    m_shell->create();
+    m_display->createShm();
+    OutputInterface *output = m_display->createOutput(m_display);
+    output->setPhysicalSize(QSize(269, 202));
+    const QSize windowSize(1024, 768);
+    output->addMode(windowSize);
+    output->create();
+    SeatInterface *seat = m_display->createSeat();
+    seat->setHasKeyboard(true);
+    seat->setHasPointer(true);
+    seat->setName(QStringLiteral("testSeat0"));
+    seat->create();
+}
+
+QTEST_GUILESS_MAIN(DaveTest);
+
+#include "main.moc"
